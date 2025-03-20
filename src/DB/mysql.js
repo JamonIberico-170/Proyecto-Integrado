783e46f73e8721242c6c04a8d38a89e35ab0f329
JamonIberico-170 <jamoniberico170job@gmail.com>
2025-03-20 23:25:16 +0100

    Pruebas con una imagen distinta con mysql y phpadmin, configuración de las variables de entorno, implementación de rutas y respuestas

---
+++ src/DB/mysql.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+const config = require('../config');
+
+function todos(table){
+    return "Se supone que devuelve TODO";
+}
+
+
+module.exports ={
+    todos
+}
--- src/index.js
+++ src/index.js
@@ -11,20 +11,32 @@ console.log(`Server on port ${3000}`) */
 
 const app = require('./app');
 const {createPool} = require('mysql2/promise');
+const video=require('./modulos/videos/route');
+const config = require('./config');
+
+
 const pool = createPool({
-    host : 'database',
-    user : 'root',
-    password : 'root',
+    host : config.mysql.host,
+    user : config.mysql.user,
+    password : config.mysql.password,
     port : 3306
 });
+
+
+app.use('/',video);
+
 app.get('/world', (req,res) => {
     res.send("Hello World");
 });
+
 app.get('/time', async (req,res) => {
-    const result= await pool.query('SELECT NOW()');
+    const result= await pool.query('SELECT NOW() ');    
     res.json(result[0]);
 });
+
+//Puerte en el cual la app permanece en escucha.
 app.listen(app.get('port'), () => {
     console.log("Servidor en escucha por el puerto", app.get("port"));
+    
 });
 
